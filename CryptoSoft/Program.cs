ec05c228d328509ffa6f930fffe3cbeafec79ef8
clementhellot <clement.hellot@viacesi.fr>
2021-12-01 12:21:30 +0100

    add config file

--- CryptoSoft/Program.cs
+++ CryptoSoft/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Configuration;
+using System.Collections.Specialized;
 
 namespace CryptoSoft
 {
@@ -6,7 +10,38 @@ namespace CryptoSoft
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(args);
+            if (args.Count() != 2)
+                Environment.Exit(-1);
+
+            
         }
+
+        public string LoadKey()
+        {
+            ConfigurationSettings
+
+            return "";
+
+        }
+
+        public FileInfo GetFile(string path)
+        {
+            FileInfo file = new FileInfo(path);
+            if (!file.Exists)
+                Environment.Exit(-1);
+            return file;
+        }
+
+        public string Cipher()
+        {
+            return "";
+        }
+
+        public string Uncipher()
+        {
+            return "";
+        }
+
+
     }
 }
